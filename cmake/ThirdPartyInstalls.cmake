6163148b4bf1cfc91b498d268128529f5034c000
Scott Baldric <ScottMBaldric@gmail.com>
2018-11-30 19:18:56 -0600

    Adding third party installs

---
+++ cmake/ThirdPartyInstalls.cmake
@@ -0,0 +1,4 @@
+include_guard(GLOBAL)
+
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/externals/glad/lib/cmake/glad)
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/externals/glfw/lib/cmake/glfw3)
