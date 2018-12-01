76e62f5efdfdc869ab2a12984bca346e6b8ddc63
Scott Baldric <ScottMBaldric@gmail.com>
2018-12-01 09:48:38 -0600

    Adding solution to first question.

--- cmake/CompileOptionsTarget.cmake
+++ cmake/CompileOptionsTarget.cmake
@@ -1,8 +1,8 @@
 include_guard(GLOBAL)
 
-add_library(logl_CompileOptions INTERFACE)
+add_library(ogl_CompileOptions INTERFACE)
 
-target_compile_options(logl_CompileOptions
+target_compile_options(ogl_CompileOptions
 	INTERFACE
 		/W4 /WX /MP
 )
---
+++ examples/getting_started/hello_triangle_01.cpp
@@ -0,0 +1,73 @@
+#include <glad/glad.h>
+
+#include <ogl/Buffer.h>
+#include <ogl/Program.h>
+#include <ogl/RenderWindow.h>
+#include <ogl/VertexArray.h>
+
+#include <ogl/utils/MainMacro.h>
+#include <ogl/utils/ProgramFactory.h>
+#include <ogl/utils/ScopedBind.h>
+
+#include <string>
+
+std::string vertex_source = R"(
+#version 330 core
+
+layout (location = 0) in vec4 pos;
+
+void main()
+{
+    gl_Position = pos;
+})";
+
+std::string frag_source = R"(
+#version 330 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+})";
+
+class HelloTriangle : public ogl::RenderWindow
+{
+public:
+    HelloTriangle() :
+        prog(ogl::utils::createProgram(vertex_source, frag_source))
+    {
+    }
+
+private:
+    ogl::Buffer triangle_data;
+    ogl::Program prog;
+    ogl::VertexArray vao;
+    
+    void init() override
+    {
+        ogl::utils::ScopedBind bind_vao(vao);
+		ogl::utils::ScopedBind bind_buffer(triangle_data);
+        triangle_data.load_data<float>({
+            -0.8f, -0.8f, 0.0f,
+            -0.4f, 0.6f, 0.0f,
+            0.0f, -0.8f, 0.0f,
+            0.0f, -0.8f, 0.0f,
+            0.8f, -0.8f, 0.0f,
+            0.4f, 0.6f, 0.0f,
+        });
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+    }
+
+    void render() override
+    {
+        ogl::utils::ScopedBind bind_vao(vao);
+        ogl::utils::ScopedBind bind_program(prog);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+};
+
+RUN_PROGRAM(HelloTriangle)
