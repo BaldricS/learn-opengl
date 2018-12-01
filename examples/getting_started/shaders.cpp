1409d89b368552ad1c15fb93db69afcb7b5df142
Scott Baldric <ScottMBaldric@gmail.com>
2018-12-01 14:36:41 -0600

    Adding shaders example.

---
+++ examples/getting_started/shaders.cpp
@@ -0,0 +1,77 @@
+#include <glad/glad.h>
+
+#include <ogl/Buffer.h>
+#include <ogl/Program.h>
+#include <ogl/RenderWindow.h>
+#include <ogl/Uniform.h>
+#include <ogl/VertexArray.h>
+
+#include <ogl/utils/MainMacro.h>
+#include <ogl/utils/ProgramFactory.h>
+#include <ogl/utils/ScopedBind.h>
+
+#include <string>
+
+std::string vertex_source = R"(
+#version 330 core
+
+layout (location = 0) in vec4 pos;
+
+void main()
+{
+    gl_Position = pos;
+})";
+
+std::string frag_source = R"(
+#version 330 core
+
+out vec4 FragColor;
+
+uniform vec4 ourColor;
+
+void main()
+{
+    FragColor = ourColor;
+})";
+
+class App : public ogl::RenderWindow
+{
+public:
+    App() :
+        prog(ogl::utils::createProgram(vertex_source, frag_source)),
+        color(prog, "ourColor")
+    {
+    }
+
+private:
+    ogl::Buffer triangle_data;
+    ogl::Program prog;
+    ogl::Uniform<float, 4> color; // Order dependency with prog
+    ogl::VertexArray vao;
+    
+    void init() override
+    {
+        ogl::utils::ScopedBind bind_vao(vao);
+		ogl::utils::ScopedBind bind_buffer(triangle_data);
+        triangle_data.load_data<float>({
+            0.5f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, 0.5f, 0.0f
+        });
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+    }
+
+    void render() override
+    {
+        ogl::utils::ScopedBind bind_vao(vao);
+        ogl::utils::ScopedBind bind_program(prog);
+
+        color.set(0.5f, 0.0f, 0.0f, 1.0f);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+};
+
+RUN_PROGRAM(App)
--- include/ogl/Uniform.h
+++ include/ogl/Uniform.h
@@ -12,21 +12,21 @@ namespace ogl
 {
     class Program;
 
-    template<template Scalar, int Size = 1>
+    template<typename Scalar, int Size = 1>
     class Uniform
     {
     public:
         Uniform(Program const & program, std::string const & uniformName) :
-            uniformLoc(glGetUniformLocation(program, uniformName.c_str()))
+            uniformLoc(glGetUniformLocation(program.handle(), uniformName.c_str()))
         {
-            if (uniformLocation == -1)
+            if (uniformLoc == -1)
             {
-                throw std::invalid_argument("Uniform " + uniformName + " not found for program " + std::to_string(p.handle()));
+                throw std::invalid_argument("Uniform " + uniformName + " not found for program " + std::to_string(program.handle()));
             }
         }
 
-        std::enable_if_t<std::is_floating_point_v<Scalar> && Size == 4>
-        set(float x, float y, float z, float w)
+        template<typename = std::enable_if_t<std::is_floating_point_v<Scalar> && Size == 4>>
+        void set(float x, float y, float z, float w)
         {
             glUniform4f(uniformLoc, x, y, z, w);
         }
